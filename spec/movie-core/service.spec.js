7da53f27395c2e74f80c277c45fe3e1382b06594
Mauricio Junior <mjuniior92@gmail.com>
2016-05-12 23:55:03 -0300

    add more tests

--- spec/movie-core/service.spec.js
+++ spec/movie-core/service.spec.js
@@ -1,30 +1,70 @@
-describe('MovieCore', function() {
-	var $httpBackend
-	var PopularMovies
+describe('MovieCore', function () {
+  var $httpBackend
+  var PopularMovies
 
-	beforeEach(module('MovieCore'))
-	beforeEach(inject(function (_PopularMovies_, _$httpBackend_) {
-		PopularMovies = _PopularMovies_
-		$httpBackend = _$httpBackend_
-	}))
-	afterEach(function () {
-		$httpBackend.verifyNoOutstandingExpectation()
-	})
+  beforeEach(module('MovieCore'))
+  beforeEach(inject(function (_PopularMovies_, _$httpBackend_) {
+    PopularMovies = _PopularMovies_
+    $httpBackend = _$httpBackend_
+  }))
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation()
+  })
 
-	it('should create popular movie', function(done) {
-		var expectedData = { movieId: 'tt0076759', description: 'Great movie!' }
+  it('should create popular movie', function (done) {
+    var expectedData = { movieId: 'tt0076759', description: 'Great movie!' }
 
-		$httpBackend
-			.expectPOST(/./, expectedData)
-			.respond(201)
+    $httpBackend
+      .expectPOST(/./, expectedData)
+      .respond(201)
 
-		var popularMovie = new PopularMovies({
-			movieId: 'tt0076759',
-			description: 'Great movie!'
-		})
-		popularMovie.$save()
+    var popularMovie = new PopularMovies({
+      movieId: 'tt0076759',
+      description: 'Great movie!'
+    })
+    popularMovie.$save()
 
-		expect($httpBackend.flush).not.toThrow()
-		done()
-	})
+    expect($httpBackend.flush).not.toThrow()
+    done()
+  })
+
+  it('should get popular movie by id', function (done) {
+    $httpBackend
+      .expectGET('popular/tt0076759')
+      .respond(200)
+
+    PopularMovies
+      .get({ movieId: 'tt0076759' })
+
+    expect($httpBackend.flush).not.toThrow()
+    done()
+  })
+
+  it('should update popular movie', function (done) {
+    $httpBackend
+      .expectPUT('popular')
+      .respond(200)
+
+    var popularMovie = new PopularMovies({
+      movieId: 'tt0076759',
+      description: 'Great movie!'
+    })
+
+    popularMovie.$update()
+    expect($httpBackend.flush).not.toThrow()
+    done()
+  })
+
+  it('should authenticate requests', function (done) {
+    var expectedHeaders = { authToken: 'teddybear', Accept: 'application/json, text/plain, */*' }
+    $httpBackend
+      .expectGET('popular/tt0076759', expectedHeaders)
+      .respond(200)
+
+    PopularMovies
+      .get({ movieId: 'tt0076759' })
+
+    $httpBackend.flush()
+    done()
+  })
 })
--- src/movie-core/service.js
+++ src/movie-core/service.js
@@ -1,16 +1,34 @@
-(function() {
-	'use strict'
+;(function () {
+  'use strict'
 
-	angular
-		.module('MovieCore', ['ngResource'])
-		.factory('PopularMovies', PopularMovies)
+  angular
+    .module('MovieCore', ['ngResource'])
+    .factory('PopularMovies', PopularMovies)
 
-		PopularMovies.$injector = ['$resource']
-		function PopularMovies ($resource) {
-			return $resource('popular/:movieId', { movieId: '@id' }, {
-				update: {
-					method: 'PUT'
-				}
-			})
-		}
+  PopularMovies.$injector = ['$resource']
+  function PopularMovies ($resource) {
+    var token = 'teddybear'
+    return $resource('popular/:movieId', { movieId: '@id' }, {
+      update: {
+        method: 'PUT',
+        headers: { 'authToken': token }
+      },
+      get: {
+        method: 'GET',
+        headers: { 'authToken': token }
+      },
+      query: {
+        method: 'GET',
+        headers: { 'authToken': token }
+      },
+      save: {
+        method: 'POST',
+        headers: { 'authToken': token }
+      },
+      remove: {
+        method: 'DELETE',
+        headers: { 'authToken': token }
+      },
+    })
+  }
 })()
