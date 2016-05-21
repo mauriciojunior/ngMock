7dd4efae98ea10914415c165a2bf0bfbc4a4d682
Mauricio Junior <mjuniior92@gmail.com>
2016-05-21 01:09:59 -0300

    refactoring

--- spec/movie-app/movie-result.directive.spec.js
+++ spec/movie-app/movie-result.directive.spec.js
@@ -2,25 +2,39 @@ describe('Movie Result Directive', function () {
   var result = {
     Poster: 'http://localhost/image.jpg',
     Title: 'Star Wars: Episode IV - A New Hope',
-    Director: 'George Lucas',
     Actors: 'Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing',
+    Director: 'George Lucas',
     Released: '25 May 1977',
     Genre: 'Action, Adventure, Fantasy'
   }
+
+  var expectedHTML = [
+    '<div class="col-sm-4">',
+    	'<img data-ng-src="http://localhost/image.jpg" alt="Star Wars: Episode IV - A New Hope" width="220" src="http://localhost/image.jpg">',
+    '</div>',
+    '<div class="col-sm-8">',
+	    '<h3 class="ng-binding">Star Wars: Episode IV - A New Hope</h3>',
+	    '<p class="ng-binding"><strong>Director:</strong> George Lucas</p>',
+	    '<p class="ng-binding"><strong>Actors:</strong> Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing</p>',
+	    '<p class="ng-binding"><strong>Released:</strong> 25 May 1977</p>',
+	    '<p class="ng-binding"><strong>Genre:</strong> Action, Adventure, Fantasy</p>',
+    '</div>'
+  ].join('')
   var $compile
 
   beforeEach(module('movieApp'))
 
-  beforeEach(inject(function (_$compile_) {
+  beforeEach(inject(function (_$compile_, _$rootScope_) {
     $compile = _$compile_
+    $rootScope = _$rootScope_
   }))
 
   it('should output movie result to expected HTML format', function (done) {
-    var scope = {}
-    var html
-    scope.result = result
-    html = $compile('<movie-result result="result"></movie-result>')(scope).html()
-    expect(html).toBe('<div>Star Wars: Episode IV - A New Hope</div>')
+    $rootScope.result = result
+    var element
+    element = $compile('<movie-result result="result"></movie-result>')($rootScope)
+    $rootScope.$digest()
+    expect(element.html()).toBe(expectedHTML)
 
     done()
   })
--- src/movie-app/movie-result.directive.js
+++ src/movie-app/movie-result.directive.js
@@ -10,7 +10,20 @@
         scope: {
           result: '=result'
         },
-        template: '<div>{{ result.Title }}</div>'
+        template: [
+          '<div class="row">',
+          	'<div class="col-sm-4">',
+          		'<img data-ng-src="{{ result.Poster }}" alt="{{ result.Title }}" width="220">',
+          	'</div>',
+          	'<div class="col-sm-8">',
+          		'<h3>{{ result.Title }}</h3>',
+		          '<p><strong>Director:</strong> {{ result.Director }}</p>',
+		          '<p><strong>Actors:</strong> {{ result.Actors }}</p>',
+		          '<p><strong>Released:</strong> {{ result.Released }}</p>',
+		          '<p><strong>Genre:</strong> {{ result.Genre }}</p>',
+          	'</div>',
+          '</div>'
+        ].join('')
       }
     })
 })()
